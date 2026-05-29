2f99a8c494a37532ad489cbbff4f26aa913e5a73
mario-gijon <mgijon@ujaen.es>
2026-05-29 23:05:07 +0200

    Clean ordering issue creation module

--- Backend/modules/issues/creation/issueCreation.ordering.js
+++ Backend/modules/issues/creation/issueCreation.ordering.js
@@ -5,23 +5,23 @@ export const applyIssueCreationOrdering = ({
   createdAlternatives,
   leafCriteria,
 }) => {
-  issue.alternativeOrder = createdAlternatives
+  const orderedAlternatives = createdAlternatives
     .slice()
-    .sort((a, b) => compareNameId(a.name, a._id, b.name, b._id))
-    .map((alternative) => alternative._id);
+    .sort((a, b) => compareNameId(a.name, a._id, b.name, b._id));
 
-  issue.leafCriteriaOrder = leafCriteria
-    .slice()
-    .sort((a, b) => compareNameId(a.name, a._id, b.name, b._id))
-    .map((criterion) => criterion._id);
+  issue.alternativeOrder = orderedAlternatives
+    .map((alternative) => alternative._id);
 
   const orderedLeafCriteria = leafCriteria
     .slice()
     .sort((a, b) => compareNameId(a.name, a._id, b.name, b._id));
+
+  issue.leafCriteriaOrder = orderedLeafCriteria
+    .map((criterion) => criterion._id);
+
   const criterionNames = orderedLeafCriteria.map((criterion) => criterion.name);
 
   return {
-    orderedLeafCriteria,
     criterionNames,
     isSingleLeafCriterion: criterionNames.length === 1,
   };
